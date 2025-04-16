1c8f2ae8ed6f2f7935ee3caef31432d1658c0b4f
Mariogarcia55 <alburezandre123@gmail.com>
2025-04-15 18:10:33 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,68 @@
-x=4
-while True:
-    #"la pregunta en el bucle para quue se repita al momento de fallar"
-    p=int(input("que numero crees que es "))
-    if p==x:
-       print("el numero el correcto") 
-       break
-    elif p>x:
-        print("el numero es menor")
-        
-    elif p<x:
-        print("el numero es mayor")
-    elif p== "salir":
-        break   
+import discord 
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot=commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'tu bot {bot.user} esta en linea')
+    
+@bot.command()
+async def cold(ctx):
+
+    await ctx.send("\U0001F4D4")
+
+@bot.command()
+async  def repetir(ctx, *, mensaje: str):
+    await ctx.send(mensaje)
+
+@bot.command()
+async def saludo(ctx,*, mensaje: str):
+    
+    mensaje = mensaje.lower().strip()
+
+    if "hola" in mensaje:
+        await ctx.send("Hola! ¿Cómo estás?")
+    
+    elif "Que onda" in mensaje:
+        await ctx.send("¡Hola , que tral!")
     else:
-        print("vuelve a intentarlo")
+        await ctx.send("No entendi tu saludo, pero hola!")
+
+@bot.command()
+async def messi(ctx):
+    await ctx.send("Es el mejor jugador de la historia")
+
+@bot.command()
+async def despedida(ctx,*, m: str):
+    
+    m = m.lower().strip()
+
+    if "adios" in m:
+        await ctx.send("Que te vaya bien!")
+    
+    elif "chao" in m:
+        await ctx.send("adiosssss")
+    else:
+        await ctx.send("No entendi tu mensaje, pero adios!")
+
+@bot.command()
+async def suma(ctx, a: int, b: int):
+    resultado = a + b
+    await ctx.send(f"El resultado de la suma es: {resultado}")
+
+@bot.command()
+async def divicion(ctx, a: int, b: int):
+    if b != 0:
+        res = a / b
+        await ctx.send(f"El resultado de la division es: {res}")
         
+    else:
+        await ctx.send("No se puede dividir entre cero.")
+
+token=""
+
+bot.run(token)
